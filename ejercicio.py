dc679991d01d774512921a6cfc8f145ed6e7f7fe
drax1425 <136837858+drax1425@users.noreply.github.com>
2023-06-16 12:46:32 -0400

    Add files via upload

---
+++ ejercicio.py
@@ -0,0 +1,24 @@
+l_bebidad = []
+l_platos = []
+l_postres = []
+while True:
+    print("""MENU PRINCIPAL:
+    ------------------------------------------
+    1. Ver:
+    2. Reserva:
+    3. Carta:
+          carta
+            1.bebidas(3)
+            2.platos(3)
+            3.postre(3)
+            4.Pedir
+            5.Cancelar
+
+    4.Pagar: imprimir voleta
+    5.Cancelar:
+    6.Salir:
+    ------------------------------------------""")
+    while True:
+        opciones = int("ingrese una de las opciones :")
+        if opciones == 1:
+            pass
